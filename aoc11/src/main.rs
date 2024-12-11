07a56b0e2410aff6d201fc58d08fee2ded6bfeb0
Raoul1808 <raoulthegeek@gmail.com>
2024-12-11 07:54:58 +0100

    Turn it into an iterator

--- aoc11/src/main.rs
+++ aoc11/src/main.rs
@@ -6,24 +6,23 @@ fn main() {
         .map(|s| s.parse().expect("not a number"))
         .collect();
     println!("No blinks: {} stones", stones.len());
-    let mut next = vec![];
-    for i in 0..75 {
-        for stone in &stones {
-            if *stone == 0 {
-                next.push(1);
-                continue;
-            }
-            let stone_str = stone.to_string();
-            if stone_str.len() % 2 == 0 {
-                let mid = stone_str.len() / 2;
-                next.push(stone_str[..mid].parse().expect("not a number"));
-                next.push(stone_str[mid..].parse().expect("not a number"));
-            } else {
-                next.push(*stone * 2024);
-            }
-        }
-        stones = next;
-        next = vec![];
+    for i in 0..25 {
+        stones = stones.into_iter()
+            .fold(vec![], |mut next, stone| {    
+                if stone == 0 {
+                    next.push(1);
+                    return next;
+                }
+                let stone_str = stone.to_string();
+                if stone_str.len() % 2 == 0 {
+                    let mid = stone_str.len() / 2;
+                    next.push(stone_str[..mid].parse().expect("not a number"));
+                    next.push(stone_str[mid..].parse().expect("not a number"));
+                } else {
+                    next.push(stone * 2024);
+                }
+                next
+            });
         println!("Blink {}, there are {} stones", i + 1, stones.len());
     }
 }
