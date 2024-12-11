e41e8ab67478b8f5fd4ae51f2a1fcf82d2cf21e5
Raoul1808 <raoulthegeek@gmail.com>
2024-12-11 07:49:56 +0100

    AoC11 First star

--- aoc11/src/main.rs
+++ aoc11/src/main.rs
@@ -1,9 +1,29 @@
 fn main() {
     let contents = include_str!("../input");
-    let stones: Vec<i32> = contents
+    let mut stones: Vec<i64> = contents
         .trim()
         .split(" ")
         .map(|s| s.parse().expect("not a number"))
         .collect();
-    println!("Stones: {:?}", stones);
+    println!("No blinks: {} stones", stones.len());
+    let mut next = vec![];
+    for i in 0..75 {
+        for stone in &stones {
+            if *stone == 0 {
+                next.push(1);
+                continue;
+            }
+            let stone_str = stone.to_string();
+            if stone_str.len() % 2 == 0 {
+                let mid = stone_str.len() / 2;
+                next.push(stone_str[..mid].parse().expect("not a number"));
+                next.push(stone_str[mid..].parse().expect("not a number"));
+            } else {
+                next.push(*stone * 2024);
+            }
+        }
+        stones = next;
+        next = vec![];
+        println!("Blink {}, there are {} stones", i + 1, stones.len());
+    }
 }
