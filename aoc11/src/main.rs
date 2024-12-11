58c4097b7accb92f624f1c96001fac20d5fe2439
Raoul1808 <raoulthegeek@gmail.com>
2024-12-11 07:29:09 +0100

    Parse input

--- aoc11/src/main.rs
+++ aoc11/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let contents = include_str!("../input");
+    let stones: Vec<i32> = contents
+        .trim()
+        .split(" ")
+        .map(|s| s.parse().expect("not a number"))
+        .collect();
+    println!("Stones: {:?}", stones);
 }
